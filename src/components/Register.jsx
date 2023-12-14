2e417a956b1fcdfe2a7e6f3e1324f4d8b7d7b4f8
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2023-12-14 20:43:43 -0300

    pruebaaa

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./register.css";
 
 function Register() {
+  console.log("test")
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
     firstName: "",
