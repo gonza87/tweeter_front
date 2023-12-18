deaca6e87488f0dd37972c7ad61da577ca147229
mariosilveira1980 <139134070+mariosilveira1980@users.noreply.github.com>
2023-12-18 14:25:10 -0300

    control de texto en tw home y tw profile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -227,7 +227,7 @@ const TweetList = () => {
                       <span>@{tweet.user.username}</span>
                       {/* {getFormattedTimestamp(tweet.date)} */}
                     </div>
-                    <div className="tweet-content">
+                    <div className="tweet-content" style={{ wordBreak: 'break-word' }}>
                       {tweet.text}
                       <div className="tweet-actions">
                         <FontAwesomeIcon
