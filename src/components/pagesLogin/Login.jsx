884ea3a55aa425c1491490838a0eb0b9b7eb4251
Gonzalo Viglioni <122325178+gonza87@users.noreply.github.com>
2023-12-22 00:07:43 -0300

    Update Login.jsx

--- src/components/pagesLogin/Login.jsx
+++ src/components/pagesLogin/Login.jsx
@@ -11,7 +11,7 @@ import "./login.css";
 function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const apiUrl = "test"//import.meta.env.VITE_API_URL+"token";
+  const apiUrl = "https://tweeter-api-one.vercel.app/token"//import.meta.env.VITE_API_URL+"token";
 
   const [loginData, setLoginData] = useState({
     emailOrUsername: "",
